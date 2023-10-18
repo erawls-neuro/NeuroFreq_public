28d10a8250766a963127b239e5296e9d82cc8aeb
erawls-neuro <erawls89@gmail.com>
2023-10-18 13:00:22 -0500

    change GPL text to v3

--- transforms/nf_ridrihaczek.m
+++ transforms/nf_ridrihaczek.m
@@ -36,7 +36,7 @@ function tfRes = nf_ridrihaczek(data,Fs,fRes,cwkernel,makePos,plt)
 % 
 % This program is free software; you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
-% the Free Software Foundation; either version 2 of the License, or
+% the Free Software Foundation; either version 3 of the License, or
 % (at your option) any later version.
 %
 % This program is distributed in the hope that it will be useful,
--- transforms/nf_stft.m
+++ transforms/nf_stft.m
@@ -37,7 +37,7 @@ function tfRes = nf_stft(data,Fs,window,overlap,fRes,plt)
 % 
 % This program is free software; you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
-% the Free Software Foundation; either version 2 of the License, or
+% the Free Software Foundation; either version 3 of the License, or
 % (at your option) any later version.
 %
 % This program is distributed in the hope that it will be useful,
